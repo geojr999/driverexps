2e727545d7e0322557c53996bac65da8ff4b8c27
Geo Joseph <geo.joseph@mistralsolutions.com>
2016-08-21 23:33:23 +0530

    read write fops and userspace code added jiffies testy

--- test.c
+++ test.c
@@ -1,15 +1,135 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/fs.h>
+#include <linux/errno.h>
+#include <asm/uaccess.h>
+#include <linux/version.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
+#include <linux/jiffies.h>
+
+static dev_t first;        // Global variable for the first device number
+static struct cdev c_dev;  // Global variable for the character device structure
+static struct class *cl;   // Global variable for the device class
+
+static int init_result;
+
+
+static ssize_t dummy_read( struct file* F, char *buf, size_t count, loff_t *f_pos )
+{
+	unsigned long j, stamp_1, stamp_half, stamp_n;
+	j = jiffies;                      /* read the current value */
+	stamp_1    = j + HZ;              /* 1 second in the future */
+	stamp_half = j + HZ/2;            /* half a second */
+	stamp_n    = j + 55555 * HZ / 1000;   /* n milliseconds */   	
+	 printk("j %lu, stamp_1 %lu, stamp_half %lu, stamp_n %lu\n",j, stamp_1, stamp_half, stamp_n);
+
+	return 0;
+}
+
+static ssize_t dummy_write( struct file* F, const char *buf, size_t count, loff_t *f_pos )
+{
+ 
+    printk(KERN_INFO "Executing WRITE.\n");
+ 
+    switch( buf[0] )
+    {
+        case '0':
+    	printk("Executing WRITE.0\n");
+        break;
+ 
+        case '1':
+   	 printk("Executing WRITE. 1\n");
+        break;
+ 
+        default:
+        printk("Wrong option.\n");
+        break;
+    }
+    return count;
+}
+
+
+
+static int dummy_open( struct inode *inode, struct file *file )
+{
+    return 0;
+}
+ 
+static int dummy_close( struct inode *inode, struct file *file )
+{
+    return 0;
+}
+
+static struct file_operations FileOps =
+{
+    .owner        = THIS_MODULE,
+    .open         = dummy_open,
+    .read         = dummy_read,
+    .write        = dummy_write,
+    .release      = dummy_close,
+};
 
 static int __init hello_world( void )
 {
-  printk( "hello world!\n" );
-  return 0;
+ 
+    init_result = alloc_chrdev_region( &first, 0, 1, "dummy_drv" );
+ 
+    if( 0 > init_result )
+    {
+        printk( KERN_ALERT "Device Registration failed\n" );
+        return -1;
+    }
+    //else
+    //{
+    //    printk( KERN_ALERT "Major number is: %d\n",init_result );
+    //    return 0;
+    //}
+ 
+    if ( (cl = class_create( THIS_MODULE, "chardev" ) ) == NULL )
+    {
+        printk( KERN_ALERT "Class creation failed\n" );
+        unregister_chrdev_region( first, 1 );
+        return -1;
+    }
+ 
+    if( device_create( cl, NULL, first, NULL, "dummy_drv" ) == NULL )
+    {
+        printk( KERN_ALERT "Device creation failed\n" );
+        class_destroy(cl);
+        unregister_chrdev_region( first, 1 );
+        return -1;
+    }
+ 
+    cdev_init( &c_dev, &FileOps );
+ 
+    if( cdev_add( &c_dev, first, 1 ) == -1)
+    {
+        printk( KERN_ALERT "Device addition failed\n" );
+        device_destroy( cl, first );
+        class_destroy( cl );
+        unregister_chrdev_region( first, 1 );
+        return -1;
+    }
+ 
+    return 0;
+
+//  printk( "hello world!\n" );
+//  return 0;
 }
 
 static void __exit goodbye_world( void )
 {
-  printk( "goodbye world!\n" );
+	cdev_del( &c_dev );
+    	device_destroy( cl, first );
+    	class_destroy( cl );
+    	unregister_chrdev_region( first, 1 );
+ 
+    printk(KERN_ALERT "Device unregistered\n"); 
 }
 
 module_init( hello_world );
